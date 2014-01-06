4739df62057e3bc1ab24d9e36f89ba25fad6dfb2
Srinivasa Kumar Meda <sree.meda@gmail.com>
2014-01-06 15:33:22 -0800

    @ProvidedBy Example

--- LearnGuice/src/com/learn/guice/learning/concepts/TransactionLog.java
+++ LearnGuice/src/com/learn/guice/learning/concepts/TransactionLog.java
@@ -1,5 +1,10 @@
 package com.learn.guice.learning.concepts;
 
+import com.google.inject.ProvidedBy;
+import com.learn.guice.learning.concepts.providers.DatabaseTransactionLogProvider;
+
+@ProvidedBy(DatabaseTransactionLogProvider.class)	// This is equivalent to defining the provider 
+													// 			bind(TransactionLog.class).toProvider(DatabaseTransactionLogProvider.class); in Guice's Configuration Module.
 public interface TransactionLog {
 	public String type();
 }
