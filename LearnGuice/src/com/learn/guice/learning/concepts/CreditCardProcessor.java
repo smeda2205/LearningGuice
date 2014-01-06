fe96a29c888ff9caaf8588d939466291bfd31bc0
Srinivasa Kumar Meda <sree.meda@gmail.com>
2014-01-06 15:29:34 -0800

    @ImplementedBy Example

--- LearnGuice/src/com/learn/guice/learning/concepts/CreditCardProcessor.java
+++ LearnGuice/src/com/learn/guice/learning/concepts/CreditCardProcessor.java
@@ -1,5 +1,10 @@
 package com.learn.guice.learning.concepts;
 
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(PaypalCreditCardProcessor.class) //Provides default implementation, can be overriden by the bind() method in Guice's Configuration Module.
+												// This is equivalent to defining 
+												//			bind(CreditCardProcessor.class).to(PayPalCreditCardProcessor.class); in guice's configuration module.
 public interface CreditCardProcessor {
 	public String getProcessorName();
 }
